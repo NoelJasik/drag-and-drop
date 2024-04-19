e69520e606663687e610e15c3e93960aaecb259f
NoelJasik <Noeloskar99@proton.me>
2024-04-19 12:23:06 +0200

    Added animations, and dragging of the whole rectangle

--- Assets/Scripts/ActionPointsConsumer.cs
+++ Assets/Scripts/ActionPointsConsumer.cs
@@ -4,24 +4,29 @@ using TMPro;
 using UnityEngine;
 using UnityEngine.EventSystems;
 using UnityEngine.UI;
-public class ActionPointsConsumer : MonoBehaviour, IDropHandler
+public class ActionPointsConsumer : MonoBehaviour, IDropHandler, IPointerEnterHandler, IPointerExitHandler
 {
     [SerializeField]
     private TextMeshProUGUI actionValueText;
     public int requiredActionPoints = 10;
     public bool isCompleted = false;
+    public bool isHovered = false;
     
     
     Image image;
     [SerializeField] Sprite unselectedSprite;
     [SerializeField] Sprite selectedSprite;
     [SerializeField] Sprite completedSprite;
+    [SerializeField] private Sprite hoverSprite;
     [SerializeField] GameObject checkMark;
     [SerializeField] GameObject additionEffect;
 
+    private Animator anim;
+    static string POP_TRIGGER = "Pop";
     
     private void Awake()
     {
+        anim = GetComponent<Animator>();
         image = GetComponent<Image>();
     }
     private void Start()
@@ -31,7 +36,7 @@ public class ActionPointsConsumer : MonoBehaviour, IDropHandler
 
     public void Update()
     {
-        if(isCompleted)
+        if(isCompleted || isHovered)
             return;
         if (ActionPointsHolder.IS_DRAGGING)
         {
@@ -66,16 +71,37 @@ public class ActionPointsConsumer : MonoBehaviour, IDropHandler
             ActionPointsHolder actionPointsHolder = eventData.pointerDrag.GetComponent<ActionPointsHolder>();
             if (actionPointsHolder != null)
             {
+                anim.SetTrigger(ActionPointsConsumer.POP_TRIGGER);
                 Instantiate(additionEffect, Camera.main.ScreenToWorldPoint(Input.mousePosition), Quaternion.identity);
                 ConsumeActionPoints(actionPointsHolder.actionPoints);
                 actionPointsHolder.ConsumePoints();
                 actionValueText.text = requiredActionPoints.ToString();
+                isHovered = false;
+
                 if (isCompleted)
                 {
                     Debug.Log("Completed");
                 }
             }
         }
+    }
+    
+    public void OnPointerEnter(PointerEventData eventData)
+    {
+        Debug.Log("Pointer Enter");
+        if(isCompleted || !ActionPointsHolder.IS_DRAGGING)
+            return;
+        image.sprite = hoverSprite;
+        isHovered = true;
+    }
+    
+    public void OnPointerExit(PointerEventData eventData)
+    {
+        Debug.Log("Pointer Exit");
+        if(isCompleted || !ActionPointsHolder.IS_DRAGGING)
+            return;
+        image.sprite = unselectedSprite;
+        isHovered = false;
     }
 }
 
